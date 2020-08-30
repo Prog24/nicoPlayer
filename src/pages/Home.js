d343ca2d2cac16dd9b2713efff5045eae30208c7
Prog24 <kenta@suzuki.dev>
2020-08-30 14:28:45 +0900

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,11 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import { PlayArrow, Stop } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
+
+const nicoJSLib = require("nicojs");
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,15 +27,18 @@ const Home = () => {
   const localVideo = useRef(null);
   const [isBroadcasting, setIsBroadcasting] = useState(false);
   const [localStream, setLocalStream] = useState(null);
+  const [nicoJS, setNicoJS] = useState(null);
 
   const handlePlayButton = () => {
     if (isBroadcasting) {
+      // 再生中(停止処理)
       localVideo.current.srcObject = null;
       localStream.getTracks().forEach((track) => {
         track.stop();
       });
       setIsBroadcasting(false);
     } else {
+      // 停止中(再生処理)
       navigator.mediaDevices
         .getUserMedia({
           video: {
@@ -42,11 +48,27 @@ const Home = () => {
           audio: true,
         })
         .then((stream) => {
-          setLocalStream(stream);
           localVideo.current.srcObject = stream;
+          const nico = new nicoJSLib({
+            app: document.getElementById("comment-screen"),
+            width: 500,
+            height: 300,
+            font_size: 36,
+            speed: 8,
+            color: "#FFF",
+          });
+          setNicoJS(nico);
+          nico.listen();
+          setLocalStream(stream);
           setIsBroadcasting(true);
         });
     }
+  };
+
+  const sendMessage = () => {
+    const message = document.getElementById("comment-field").value;
+    nicoJS.send({ text: message });
+    document.getElementById("comment-field").value = null;
   };
 
   return (
@@ -58,33 +80,44 @@ const Home = () => {
       </AppBar>
       <Card className={classes.root}>
         <CardContent>
-          <video
-            id="video"
-            width="100%"
-            autoPlay
-            muted
-            ref={localVideo}
-          ></video>
+          <div id="comment-screen">
+            <video
+              id="video"
+              width="500px"
+              height="300px"
+              autoPlay
+              muted
+              ref={localVideo}
+            ></video>
+          </div>
         </CardContent>
         <CardActions>
           {isBroadcasting ? (
-            <Button
-              onClick={handlePlayButton}
-              variant="contained"
-              color="secondary"
-            >
-              <Stop />
-              停止
-            </Button>
+            <div>
+              <Button
+                onClick={handlePlayButton}
+                variant="contained"
+                color="secondary"
+              >
+                <Stop />
+                停止
+              </Button>
+              <TextField id="comment-field" color="primary" label="コメント" />
+              <Button onClick={sendMessage} variant="contained" color="primary">
+                送信
+              </Button>
+            </div>
           ) : (
-            <Button
-              onClick={handlePlayButton}
-              variant="contained"
-              color="primary"
-            >
-              <PlayArrow />
-              再生
-            </Button>
+            <div>
+              <Button
+                onClick={handlePlayButton}
+                variant="contained"
+                color="primary"
+              >
+                <PlayArrow />
+                再生
+              </Button>
+            </div>
           )}
         </CardActions>
       </Card>
